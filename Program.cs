31c0e29d34e2355a767bdd5382278b2dac49382f
Alesia - Imali SuMaku Kute <73201769+AlesiaLRuffin@users.noreply.github.com>
2020-11-11 23:05:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Exercise: Month Converter
+            Console.WriteLine("Give the numeric value for the month you were born");
+            var month = Convert.ToInt32 (Console.ReadLine());
+            DateTimeFormatInfo dateTimeInfo = new DateTimeFormatInfo();
+            Console.WriteLine(dateTimeInfo.GetMonthName(month));
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+            
+        }
+    }
+}     
